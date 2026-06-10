7ef928b99e9af9d4c5953e7856595742d6aad69b
Annie-tech520 <ngunjiriannie427@gmail.com>
2026-06-10 14:11:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let fName="Anne"
+let lName="Ngunjiri"
+console.log(fName+" "+lName)
+let marks=450
+console.log(marks)
+let fTermFees=20000
+let sTermFees=30000
+console.log(marks)
+console.log(fTermFees)
+console.log(sTermFees)
+let gpa=7.5
+console.log(gpa)
+let pass=true
+console.log(pass)
+let mainName=document.getElementById("fName")
+mainName.innerHTML=fName
+let secondName=document.getElementById("lName")
+secondName.innerHTML=lName
+let thirdName=document.getElementById("marks")
+thirdName.innerHTML=marks
+let fourthName=document.getElementById("gpa")
+fourthName.innerHTML=gpa
+let fifthName=document.getElementById("fTermFees")
+fifthName.innerHTML=fTermFees
+let sixthName=document.getElementById("sTermFees")
+sixthName.innerHTML=sTermFees
+
+
+
