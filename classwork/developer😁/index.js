833335311a10a38f683c003dd49a24b753296a3e
Annie-tech520 <ngunjiriannie427@gmail.com>
2026-06-22 16:33:34 +0300

    Add files via upload

---
+++ classwork/developer😁/index.js
@@ -0,0 +1,20 @@
+let  edible=["Bread","Crisps"]
+let nonedible=["plastic cups"]
+let machinery=["motorcycle","electric saw","electric drills"];
+//Ordered output
+console.log("Edible:");
+console.log(edible)
+
+console.log("Machinery:");
+console.log(machinery)
+
+console.log("Non-Edible:");
+console.log(nonedible)
+
+document.write("Edible:" + edible+"<br>")
+document.write(" Non-Edible:" + nonedible+"<br>")
+document.write("Machinery:" + machinery )
+
+
+
+
